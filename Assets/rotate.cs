17914119c60d71bdfdfd566715531f4f5134c513
CalvinZheng <calvinz.mcgill@gmail.com>
2015-07-07 17:06:55 -0400

    first commit

---
+++ Assets/rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.RotateAround (Vector3.zero, Vector3.up, 0.01f);
+	}
+}
